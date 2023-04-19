283795d0ea3cf28a8f6229dc48340c1caf26d056
Anthony Sasso <71456585+anthonysasso2001@users.noreply.github.com>
2023-04-19 14:08:14 -0400

    created example tests

---
+++ patient_scanlines/test/main_test.dart
@@ -0,0 +1,11 @@
+// Import test and tested class
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // first example test to setup folder
+  group('Test Collection', () {
+    test('Example Test', () {
+      expect(1, 1);
+    });
+  });
+}
